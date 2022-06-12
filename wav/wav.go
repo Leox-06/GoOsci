0f0f6ab61224354cd74299d3e1ed010fe4271194
Leonard <leoxmimmo@gmail.com>
2022-06-12 15:37:17 +0200

    fixed bugs and renamed functions

--- wav/wav.go
+++ wav/wav.go
@@ -7,69 +7,93 @@ import (
 
 type Wav struct {
 	// "RIFF" chunk descriptor
-	ChunkID   uint32 // "RIFF"
-	ChunkSize uint32 // 36 + SubChunk2Size
-	Format    uint32 // "WAVE"
+	chunkID   uint32 // "RIFF"
+	chunkSize uint32 // 36 + SubChunk2Size
+	format    uint32 // "WAVE"
 
 	// "fmt" sub-chunk
-	Subchunk1ID   uint32 // "fmt "
-	Subchunk1Size uint32 // sum of the rest subchunk size (2+2+4+4+2+2=16)
-	AudioFormat   uint16 // 1 for PCM
+	subchunk1ID   uint32 // "fmt "
+	subchunk1Size uint32 // sum of the rest subchunk size (2+2+4+4+2+2=16)
+	audioFormat   uint16 // 1 for PCM
 	NumChannels   uint16 // 2 for stereo
 	SampleRate    uint32 // 48000 Hz (80 BB 00 00)
-	ByteRate      uint32 // ByteRates=(Sample Rate x Bits Per Sample x Channel Numbers)/8
-	BlockAlign    uint16 // Data block size
+	byteRate      uint32 // ByteRates=(Sample Rate x Bits Per Sample x Channel Numbers)/8
+	blockAlign    uint16 // Data block size
 	BitsPerSample uint16 // 16bits
 
 	// "data" sub-chunk
-	Subchunk2ID   uint32 // "data"
-	Subchunk2Size uint32 // Number of bytes in the data (Sample numbers x Channel numbers x Bits per sample)/8
+	subchunk2ID   uint32 // "data"
+	subchunk2Size uint32 // Number of bytes in the data (Sample numbers x Channel numbers x Bits per sample)/8
 
-	data []byte
+	Data []byte
 }
 
-func SetWav(SampleRate uint32, BitsPerSample uint16, data []byte) Wav {
+func New(NumChannels uint16, SampleRate uint32, BitsPerSample uint16) Wav {
 	var w Wav
 
-	w.ChunkID = uint32(0x52494646) // "RIFF"
-	w.ChunkSize = uint32(36 + len(data))
-	w.Format = uint32(0x57415645) // "WAVE"
+	// "RIFF" chunk descriptor
+	w.chunkID = uint32(0x52494646) // "RIFF"
+	// chunkSize
+	w.format = uint32(0x57415645) // "WAVE"
 
-	w.Subchunk1ID = uint32(0x666d7420) // "fmt "
-	w.Subchunk1Size = uint32(16)
-	w.AudioFormat = uint16(1)
-	w.NumChannels = uint16(2)
+	// "fmt" sub-chunk
+	w.subchunk1ID = uint32(0x666d7420) // "fmt "
+	w.subchunk1Size = uint32(16)
+	w.audioFormat = uint16(1)
+	w.NumChannels = NumChannels
 	w.SampleRate = SampleRate
-	w.ByteRate = (SampleRate * uint32(BitsPerSample) * uint32(w.NumChannels)) / 8
-	w.BlockAlign = (w.NumChannels * BitsPerSample) / 8
+	w.byteRate = (SampleRate * uint32(BitsPerSample) * uint32(NumChannels)) / 8
+	w.blockAlign = (NumChannels * BitsPerSample) / 8
 	w.BitsPerSample = BitsPerSample
 
-	w.Subchunk2ID = uint32(0x64617461) // "data"
-	w.Subchunk2Size = uint32(len(data))
-	w.data = data
+	// "data" sub-chunk
+	w.subchunk2ID = uint32(0x64617461) // "data"
+	// subchunk2Size
 
 	return w
 }
 
-func (w Wav) Encode() []byte {
+func (w *Wav) Encode() []byte {
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.BigEndian, w.ChunkID)
-	binary.Write(buf, binary.LittleEndian, w.ChunkSize)
-	binary.Write(buf, binary.BigEndian, w.Format)
 
-	binary.Write(buf, binary.BigEndian, w.Subchunk1ID)
-	binary.Write(buf, binary.LittleEndian, w.Subchunk1Size)
-	binary.Write(buf, binary.LittleEndian, w.AudioFormat)
+	// headers
+	// "RIFF" chunk descriptor
+	binary.Write(buf, binary.BigEndian, w.chunkID)
+	w.chunkSize = uint32(36 + len(w.Data))
+	binary.Write(buf, binary.LittleEndian, w.chunkSize)
+	binary.Write(buf, binary.BigEndian, w.format)
+	
+	// "fmt" sub-chunk
+	binary.Write(buf, binary.BigEndian, w.subchunk1ID)
+	binary.Write(buf, binary.LittleEndian, w.subchunk1Size)
+	binary.Write(buf, binary.LittleEndian, w.audioFormat)
 	binary.Write(buf, binary.LittleEndian, w.NumChannels)
 	binary.Write(buf, binary.LittleEndian, w.SampleRate)
-	binary.Write(buf, binary.LittleEndian, w.ByteRate)
-	binary.Write(buf, binary.LittleEndian, w.BlockAlign)
+	binary.Write(buf, binary.LittleEndian, w.byteRate)
+	binary.Write(buf, binary.LittleEndian, w.blockAlign)
 	binary.Write(buf, binary.LittleEndian, w.BitsPerSample)
+	
+	// "data" sub-chunk
+	binary.Write(buf, binary.BigEndian, w.subchunk2ID)
+	w.subchunk2Size = uint32(len(w.Data))
+	binary.Write(buf, binary.LittleEndian, w.subchunk2Size)
 
-	binary.Write(buf, binary.BigEndian, w.Subchunk2ID)
-	binary.Write(buf, binary.LittleEndian, w.Subchunk2Size)
-
-	binary.Write(buf, binary.LittleEndian, w.data)
+	// data
+	binary.Write(buf, binary.LittleEndian, w.Data)
 
 	return buf.Bytes()
 }
+
+// func mon2ster(mon []byte, num) []byte {
+
+// }
+
+// func (w *Wav) GenerateTone(frequency float64, amplitude float64, duration float64) {
+// 	var samples []byte
+// 	for i := 0.0; i < duration*float64(w.SampleRate); i += 1 / float64(w.SampleRate) {
+// 		sample := amplitude * math.Sin(i*2*math.Pi*frequency)
+// 		samples = append(samples, byte(sample))
+// 		// fmt.Printf("%f ---> %f\n", i, sample)
+// 	}
+// 	w.Data = append(w.Data, samples...)
+// }
